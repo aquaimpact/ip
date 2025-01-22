5c41caf2a8554dacf9de48810e09dd37f420306d
aquaimpact <oweien12@gmail.com>
2025-01-22 21:30:48 +0800

    Update Todo with methods

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+public class Todo extends Task{
+    public Todo(String name) {
+        super(name);
+    }
 
-public class Todo {
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
 }
