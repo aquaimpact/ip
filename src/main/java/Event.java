57af89ff7854e23c1bd87e2fc10cfa6c8fb23ffc
aquaimpact <oweien12@gmail.com>
2025-01-22 22:05:49 +0800

    Update Event methods

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,5 +1,13 @@
 public class Event extends Task{
-    public Event(String taskName) {
+    private String from, to;
+    public Event(String taskName, String from, String to) {
         super(taskName);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + ", to: " + to + ")";
     }
 }
