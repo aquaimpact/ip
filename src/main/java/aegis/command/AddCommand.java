8c2a33d2067f9ebd9fcda7e0fc97c50720f804fd
aquaimpact <oweien12@gmail.com>
2025-02-02 00:35:12 +0800

    Add JavaDoc to all the commands

--- src/main/java/aegis/command/AddCommand.java
+++ src/main/java/aegis/command/AddCommand.java
@@ -8,18 +8,30 @@ import aegis.task.Task;
 import aegis.task.TaskList;
 import aegis.ui.UIManager;
 
+/**
+ * Represents a command to add a task to the task list.
+ */
 public class AddCommand implements Command {
 
     private Task task;
 
+    /**
+     * Constructs an AddCommand with the specified task.
+     *
+     * @param task The task to be added.
+     */
     public AddCommand(Task task) {
         this.task = task;
     }
 
     /**
-     * @param tasks
-     * @param fs
-     * @throws TaskInputException
+     * Executes the command by adding the task to the task list, saving the updated list to storage,
+     * and displaying a confirmation message to the user.
+     *
+     * @param tasks The task list to which the task will be added.
+     * @param fs    The file storage system used to persist the task list.
+     * @throws TaskInputException If an error occurs while handling task input.
+     * @throws IOException        If an error occurs while writing to the file.
      */
     @Override
     public void execute(TaskList tasks, FileSave fs) throws TaskInputException, IOException {
@@ -29,7 +41,9 @@ public class AddCommand implements Command {
     }
 
     /**
-     * @return
+     * Indicates whether this command causes the program to exit.
+     *
+     * @return {@code false}, since adding a task does not cause the program to exit.
      */
     @Override
     public boolean isExit() {
--- src/main/java/aegis/command/DueDatesCommand.java
+++ src/main/java/aegis/command/DueDatesCommand.java
@@ -7,27 +7,41 @@ import aegis.task.TaskList;
 import aegis.task.Todo;
 import aegis.ui.UIManager;
 
+/**
+ * Represents a command that lists all tasks with due dates in ascending order.
+ * Tasks of type {@code Todo} are excluded as they do not have due dates.
+ */
 public class DueDatesCommand implements Command {
 
     /**
-     * @param tasks
-     * @param fs
-     * @throws TaskInputException
+     * Executes the command to display tasks sorted by their due dates.
+     * <p>
+     * Tasks of type {@code Todo} are ignored since they do not have due dates.
+     * The formatted list is then printed to the UI.
+     *
+     * @param tasks The task list containing tasks to be sorted and displayed.
+     * @param fs    The file storage handler (not used in this command).
+     * @throws TaskInputException If an error occurs while processing the tasks.
      */
     @Override
     public void execute(TaskList tasks, FileSave fs) throws TaskInputException {
-        String output = "Here are the upcoming duedates in your list:";
+        String output = "Here are the upcoming due dates in your list:";
 
         int index = 1;
-        for(Task t: tasks.getSortedDueDates()) {
-            if(t instanceof Todo) continue;
-            output += ("\n"+ index++ + "." + t.toString());
+        for (Task t : tasks.getSortedDueDates()) {
+            if (t instanceof Todo) {
+                continue;
+            }
+            output += ("\n" + index++ + "." + t.toString());
         }
+
         UIManager.printBorders(output);
     }
 
     /**
-     * @return
+     * Indicates whether this command causes the program to exit.
+     *
+     * @return false, since listing due dates does not terminate the program.
      */
     @Override
     public boolean isExit() {
