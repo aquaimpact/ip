7693cddf475f11e4d0b62212aa6e70b3cb3afa4f
aquaimpact <oweien12@gmail.com>
2025-02-02 01:39:02 +0800

    Add JavaDoc to each task type and fix bugs

--- src/main/java/aegis/task/Event.java
+++ src/main/java/aegis/task/Event.java
@@ -1,26 +1,30 @@
 package aegis.task;
 
-import aegis.exception.TaskInputException;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
+import aegis.exception.TaskInputException;
+
 /**
- * Represents an event task in the aegis.Aegis chatbot.
+ * Represents an event task in the Aegis chatbot.
  * An event task has a task name, a start time (from), and an end time (to).
  */
-public class Event extends Task implements Comparable{
-    private LocalDateTime from, to;
+public class Event extends Task implements Comparable<Task> {
+
     private DateTimeFormatter storeFormatter = DateTimeFormatter.ofPattern("M/d/yyyy HHmm");
     private DateTimeFormatter showFormatter = DateTimeFormatter.ofPattern("d/M/yyyy HH:mm");
+    private LocalDateTime from;
+    private LocalDateTime to;
+
     /**
-     * Constructs an aegis.task.Event object with the specified task name, start time, and end time.
+     * Constructs an Event object with the specified task name, start time, and end time.
      *
      * @param taskName The name or description of the event.
-     * @param from The start time of the event.
-     * @param to The end time of the event.
+     * @param from The start time of the event in the format "M/d/yyyy HHmm".
+     * @param to The end time of the event in the format "M/d/yyyy HHmm".
      * @throws TaskInputException If the task name, start time, or end time is invalid.
+     * @throws DateTimeParseException If the start time or end time is in an invalid format.
      */
     public Event(String taskName, String from, String to) throws TaskInputException, DateTimeParseException {
         super(taskName);
@@ -39,17 +43,31 @@ public class Event extends Task implements Comparable{
         return "[E]" + super.toString() + " (from: " + from.format(showFormatter) + " to: " + to.format(showFormatter) + ")";
     }
 
+    /**
+     * Converts the Event task to a CSV-compatible string format.
+     * The format includes the task type, completion status, task name, start time, and end time.
+     *
+     * @return A CSV-compatible string representing the event task.
+     */
     @Override
     public String toCsv() {
         return "E||" + super.toCsv() + "||" + from.format(storeFormatter) + "||" + to.format(storeFormatter);
     }
 
+    /**
+     * Compares this event task with another object (another event task or deadline).
+     * The comparison is based on the end time (the "to" field).
+     *
+     * @param o The object to be compared.
+     * @return A negative integer, zero, or a positive integer if this event's end time is earlier than,
+     *         equal to, or later than the specified object, respectively.
+     */
     @Override
-    public int compareTo(Object o) {
-        if(o instanceof Event) {
+    public int compareTo(Task o) {
+        if (o instanceof Event) {
             return ((Event) o).to.compareTo(this.to);
         } else if (o instanceof Deadline) {
-            return ((Deadline) o).compareTo(this.to);
+            return o.compareTo(this);
         }
         return 0;
     }
--- src/main/java/aegis/task/TaskList.java
+++ src/main/java/aegis/task/TaskList.java
@@ -1,27 +1,54 @@
 package aegis.task;
 
-import aegis.exception.TaskInputException;
-
 import java.util.ArrayList;
 import java.util.Collections;
 
+import aegis.exception.TaskInputException;
+
+/**
+ * Represents a list of tasks in the aegis.Aegis chatbot.
+ * A task list allows for the management of tasks, including adding, removing, and marking tasks as done or undone.
+ * It also supports sorting tasks by due date.
+ */
 public class TaskList {
     private ArrayList<Task> tasks;
 
-    public TaskList(ArrayList<Task> t){
+    /**
+     * Constructs a TaskList object with the specified list of tasks.
+     *
+     * @param t The list of tasks to initialize the TaskList.
+     */
+    public TaskList(ArrayList<Task> t) {
         tasks = t;
     }
 
+    /**
+     * Constructs an empty TaskList.
+     * Initially, the list contains no tasks.
+     */
     public TaskList() {
         tasks = new ArrayList<>();
     }
 
+    /**
+     * Adds a task to the task list.
+     *
+     * @param task The task to be added.
+     */
     public void addTask(Task task) {
         tasks.add(task);
     }
 
+    /**
+     * Retrieves the task at the specified index.
+     * Throws a TaskInputException if the index is out of bounds or if the task list is empty.
+     *
+     * @param index The index of the task to retrieve.
+     * @return The task at the specified index.
+     * @throws TaskInputException If the task list is empty or the index is invalid.
+     */
     public Task getTask(int index) throws TaskInputException {
-        if(tasks.isEmpty()) {
+        if (tasks.isEmpty()) {
             throw new TaskInputException("No task available!");
         } else if (index < 0 || index >= tasks.size()) {
             throw new TaskInputException("Invalid task index!");
@@ -29,33 +56,75 @@ public class TaskList {
         return tasks.get(index);
     }
 
+    /**
+     * Removes the task at the specified index.
+     * Throws a TaskInputException if the task list is empty or the index is invalid.
+     *
+     * @param index The index of the task to remove.
+     * @return The task that was removed.
+     * @throws TaskInputException If the task list is empty or the index is invalid.
+     */
     public Task removeTask(int index) throws TaskInputException {
-        if(tasks.isEmpty()) throw new TaskInputException("No task available!");
+        if (tasks.isEmpty()) {
+            throw new TaskInputException("No task available!");
+        }
         Task t = getTask(index); // Ensure index is valid
         tasks.remove(index);
         return t;
     }
 
+    /**
+     * Marks the task at the specified index as done.
+     * Throws a TaskInputException if the task list is empty or the index is invalid.
+     *
+     * @param index The index of the task to mark as done.
+     * @return The task that was marked as done.
+     * @throws TaskInputException If the task list is empty or the index is invalid.
+     */
     public Task markTaskAsDone(int index) throws TaskInputException {
         getTask(index).markAsDone();
         return getTask(index);
     }
 
+    /**
+     * Marks the task at the specified index as undone.
+     * Throws a TaskInputException if the task list is empty or the index is invalid.
+     *
+     * @param index The index of the task to mark as undone.
+     * @return The task that was marked as undone.
+     * @throws TaskInputException If the task list is empty or the index is invalid.
+     */
     public Task markTaskAsUndone(int index) throws TaskInputException {
         getTask(index).markAsUndone();
         return getTask(index);
     }
 
-    public  ArrayList<Task> getSortedDueDates() {
+    /**
+     * Retrieves the tasks sorted by their due dates in descending order.
+     * The tasks are sorted based on their deadlines or event end times.
+     *
+     * @return A list of tasks sorted by due dates in descending order.
+     */
+    public ArrayList<Task> getSortedDueDates() {
         ArrayList<Task> sortedTasks = new ArrayList<>(tasks);
-        Collections.sort(sortedTasks);
+        Collections.sort(sortedTasks, Collections.reverseOrder());
         return sortedTasks;
     }
 
+    /**
+     * Retrieves the list of all tasks.
+     *
+     * @return The list of tasks.
+     */
     public ArrayList<Task> getTasks() {
         return tasks;
     }
 
+    /**
+     * Retrieves the number of tasks in the task list.
+     *
+     * @return The number of tasks in the task list.
+     */
     public int getSize() {
         return tasks.size();
     }
